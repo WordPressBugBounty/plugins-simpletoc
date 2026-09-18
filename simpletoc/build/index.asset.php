c4b821bf95de21d162fb91bbc60c0d915356c3ea
Sean Murphy <sean@iamseanmurphy.com>
2026-09-18 09:40:09 +0000

    7.4.0

--- simpletoc/build/index.asset.php
+++ simpletoc/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '4f7330422e7a726302e4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'b8ed11c09acb526c3728');
